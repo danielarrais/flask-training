03e128cd62cedf1011f55503fc052f9042a5a6b4
Daniel Arrais <danielarraiscarvalho@gmail.com>
2022-11-01 21:29:03 -0300

    Implemented sign-up data validation

--- website/auth.py
+++ website/auth.py
@@ -1,9 +1,9 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, flash
 
 auth = Blueprint('auth', __name__)
 
 
-@auth.route('/login')
+@auth.route('/login', methods=['GET', 'POST'])
 def login():
     return render_template("login.html")
 
@@ -13,6 +13,24 @@ def logout():
     return "<h1>Logout</h1>"
 
 
-@auth.route('/sign-up')
+@auth.route('/sign-up', methods=['GET', 'POST'])
 def sign_up():
+    if request.method == 'POST':
+        email = request.form.get('email')
+        first_name = request.form.get('first-name')
+        password_1 = request.form.get('password1')
+        password_2 = request.form.get('password2')
+
+        if len(email) < 4:
+            flash('Email must be greater than 4 characters.', category='error')
+        elif len(first_name) < 2:
+            flash('First name must be greater than 2 characters.', category='error')
+        elif password_1 != password_2:
+            flash('Passwords don\'t match.', category='error')
+        elif len(password_1) < 7:
+            flash('Password must be greater than 7 characters.', category='error')
+        else:
+            flash('Account created!', category='success')
+    else:
+        pass
     return render_template("sign_up.html")
